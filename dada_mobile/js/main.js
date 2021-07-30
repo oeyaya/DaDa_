9d834b4fcfaee3fdd4f42650629f1d99f5a54036
oeyaya <85890292+oeyaya@users.noreply.github.com>
2021-07-31 02:30:29 +0900

    Add files via upload

---
+++ dada_mobile/js/main.js
@@ -0,0 +1,32 @@
+$(function(){
+//    slick slider 플러그인
+
+    $("#mainBanner ul").slick({
+     centerMode: true,
+  centerPadding: '120px',
+  slidesToShow: 1,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+    
+});
+    
+    
